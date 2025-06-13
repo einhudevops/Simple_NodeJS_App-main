8c6349e2a08dd78b2bf1ee530d14a977a8312b87
BhoneMyat <71804154+bhone121212@users.noreply.github.com>
2025-06-13 13:38:10 +0630

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 
 // Optional: test endpoint
 app.get('/hello', (req, res) => {
-  res.send('Hello, World! Welcome to BhoneMyatKyaw YouTube Channel.');
+  res.send('Hello, World! Welcome to BhoneMyat YouTube Channel.');
 });
 
 // Bind to 0.0.0.0 for external access
